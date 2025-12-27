ee6cda29b1e1556ee32c94bad7c0548cdcbf0c62
Nadia Mumtaz <nadiamumtazp@gmail.com>
2025-12-27 11:46:01 +0700

    update page4

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 pages = [
-    st.Page(page="pages/page1.py", title="Home", icon="🏠"),
-    st.Page(page="pages/page2.py", title="Visualisasi Data", icon="📊"),
-    st.Page(page="pages/page3.py", title="Analisis Data & Kesimpulan", icon="⚙️")
+    st.Page(page="pages/page1.py", title="Beranda", icon="🏠"),
+    st.Page(page="pages/page2.py", title="Data Interaktif", icon="📊"),
+    st.Page(page="pages/page3.py", title="Analisis Data & Kesimpulan", icon="🔬")
+    st.Page(page="pages/page4.py", title="Cara Mengunduh Data", icon="⬇️")
 ]
 
 pg = st.navigation(
@@ -11,5 +12,57 @@ pg = st.navigation(
     position="sidebar",
     expanded=True
 )
-
 pg.run()
+
+st.markdown(
+    """
+    <style>
+    /* =========================
+       KONTEN TENGAH (MAIN AREA)
+       ========================= */
+    section[data-testid="stMain"] {
+        background-color: #EBF4DD; /* hijau muda solid */
+        padding: 2rem;
+    }
+
+    /* =========================
+       SIDEBAR
+       ========================= */
+    section[data-testid="stSidebar"] {
+        background-color: #5A7863; /* hijau tua */
+    }
+
+    /* =========================
+       FONT SIDEBAR
+       ========================= */
+    section[data-testid="stSidebar"] * {
+        color: #FFFFFF !important; /* font putih */
+    }
+
+    /* Label selectbox & slider */
+    section[data-testid="stSidebar"] label {
+        color: #FFFFFF !important;
+        font-weight: 600;
+    }
+
+    /* Dropdown & input tetap readable */
+    section[data-testid="stSidebar"] select,
+    section[data-testid="stSidebar"] input {
+        color: #000000 !important;
+        background-color: #FFFFFF !important;
+        border-radius: 8px;
+    }
+
+    /* =========================
+       JUDUL KONTEN
+       ========================= */
+    h1, h2, h3 {
+        color: #2F4F3F; /* hijau gelap elegan */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
